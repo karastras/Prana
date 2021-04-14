ae14fa322e86eebabd85aac1d20f107ee1176d14
karastras <jerome.burrus.oc@gmail.com>
2021-04-14 18:48:14 +0200

    change browserouter to hashrouter to fixe issue about refresh page on Netlify

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { HashRouter} from 'react-router-dom';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router >
+    <HashRouter >
       <App />
-    </ Router>
+    </ HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
